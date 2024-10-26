4c946b1bb2c8fcd7cb95a88a466602238b1dd915
Md. Ibrahim Shipon <156697977+shipon675s@users.noreply.github.com>
2024-10-26 23:27:02 +0600

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,17 @@
+let a = 0.2;
+let b = 0.1;
+console.log(a + b);
+
+let x = 10;
+let z = 10;
+console.log(x + z);
+
+let xy = "20";
+let xz = "20";
+console.log(xy + xz); // + operator to concatenate the string.
+
+let s = "hi40";
+let v = "30";
+let n = s * v;
+console.log(s - v);
+console.log(isNaN(s));
