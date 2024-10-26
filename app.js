7af68196a6f28314773184d29a1095005f1cd332
Md. Ibrahim Shipon <156697977+shipon675s@users.noreply.github.com>
2024-10-26 23:38:30 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+//! JavaScript Numbers:
+const x = 25;
+console.log(x);
+console.log(typeof x);
+
+// string to number
+const a = 0.6;
+const b = 0.7;
+const c = (a + b).toFixed(2); // String Value
+console.log(new Number(c));
+
+const n = 100;
+const m = 200;
+const o = 300;
+console.log(n + m + o);
